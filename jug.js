bc52f3572127dab8ec556634ecca7aac9ff95f84
injelc <castilloinj@gmail.com>
2024-06-21 23:39:16 +0800

    Add files via upload

---
+++ jug.js
@@ -0,0 +1,21 @@
+const question = document.querySelector(".question")
+const gif = document.querySelector(".gif")
+const yesBtn = document.querySelector(".yes-btn")
+const noBtn = document.querySelector(".no-btn")
+
+yesBtn.addEventListener("click", ()=> {
+    question.innerHTML = "yey huggieees";
+    gif.src="https://i.pinimg.com/originals/57/e1/6f/57e16f2642326d5b8f627ea467121117.gif"
+})
+
+noBtn.addEventListener("mouseover", ()=> {
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height;
+
+    const randomX = Math.floor(Math.random() * maxX)
+    const randomY = Math.floor(Math.random() * maxY)
+
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
+})
